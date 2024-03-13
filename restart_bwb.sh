443809191e5f3f915aaa4dc4e4e3c98211628ba2
Thomas Dahlstrom <tdahlstrom@gmail.com>
2024-03-13 15:07:27 -0700

    Update restart_bwb.sh

--- restart_bwb.sh
+++ restart_bwb.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run --rm -p 6080:6080 -v /workspace/salmon-demo/:/data -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --group-add root biodepot/bwb
+docker run --rm -p 6080:6080 -v /workspace/salmon-demo/:/data -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --group-add root -e STARTING_WORKFLOW=/data/workflow/salmon_demo/salmon_demo.ows biodepot/bwb
